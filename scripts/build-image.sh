74b700ba888e2483441eb0a967d643dfe9f5cd82
caramelmelmel <melodylyq262@gmail.com>
2025-06-21 17:51:52 +0800

    change build image tag to commit sha

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 SPLIIT_APP_NAME=$(node -p -e "require('./package.json').name")
-SPLIIT_VERSION=$(node -p -e "require('./package.json').version")
+
+# Get git commit SHA - use GITHUB_SHA if in GitHub Actions, otherwise get from git
+if [ -n "$GITHUB_SHA" ]; then
+  COMMIT_SHA=$GITHUB_SHA
+else
+  COMMIT_SHA=$(git rev-parse --short HEAD)
+fi
 
 # we need to set dummy data for POSTGRES env vars in order for build not to fail
 docker buildx build \
-    -t ${SPLIIT_APP_NAME}:${SPLIIT_VERSION} \
+    -t ${SPLIIT_APP_NAME}:${COMMIT_SHA} \
     -t ${SPLIIT_APP_NAME}:latest \
     .
 
